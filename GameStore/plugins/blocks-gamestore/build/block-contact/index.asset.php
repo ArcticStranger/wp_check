335640898ba5981cc7d77dc26d36d74137eabfe9
marlysma <marlysma@student.21-school.ru>
2026-08-09 14:50:36 +0900

    plugins edit

---
+++ GameStore/plugins/blocks-gamestore/build/block-contact/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8e8f9c8708b8aa8fc975');
---
+++ GameStore/plugins/blocks-gamestore/build/block-hero/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '8ff5df93357e5b8e14dd');
